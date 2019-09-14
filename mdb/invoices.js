4e54ce84384074f99858a3f418a7911b7aadb232
Daniel Daneshi <danieldotinc@gmail.com>
2019-09-14 22:51:40 +0430

    fix default seed

---
+++ mdb/invoices.js
@@ -0,0 +1,121 @@
+const mongoose = require("mongoose");
+
+module.exports = [
+  {
+    _id: mongoose.Types.ObjectId("5d7a8d44fabfe01afc47d1c5"),
+    products: [
+      {
+        imgs: [],
+        _id: mongoose.Types.ObjectId("5d7a8cebfabfe01afc47c5ac"),
+        proCode: 1138,
+        marketCode: 767007,
+        name:
+          "كارد قصابي زنجان حسين حيدري | قهوه اي | استيل | صنايع دستي زنجان | Z",
+        description: "",
+        tradeBuyingPrice: 520000,
+        retailStoreStock: 0,
+        img: "http://www.taminmall.com/info/gateway/nepw/1138.1.jpg",
+        flag: "nikrad",
+        quantity: 2
+      },
+      {
+        imgs: [],
+        _id: mongoose.Types.ObjectId("5d7a8cebfabfe01afc47c5af"),
+        proCode: 1343,
+        marketCode: 253008,
+        name: "سرويس 6 پارچه كفگير و ملاقه  | سفيد | پلاستيك | ماي كيچن  | M",
+        description: "",
+        tradeBuyingPrice: 1230000,
+        retailStoreStock: 0,
+        img: "http://www.taminmall.com/info/gateway/nepw/1343.1.jpg",
+        flag: "nikrad",
+        quantity: 1
+      }
+    ],
+    name: "فاکتور فروش مربوط به سعید شمس",
+    invoiceType: "فاکتور فروش",
+    sellerName: "محسن شمس",
+    sellerAddress: "",
+    sellerPhoneNumber: "",
+    buyerId: mongoose.Types.ObjectId("5d7a8e03fabfe01afc47d24c"),
+    buyerName: "سعید شمس",
+    buyerAddress: "شوش، خیابان فدائیان اسلام، کوچه هفتم، پلاک 9، واحد2",
+    buyerPhoneNumber: "09122208579",
+    totalPrice: 2270000,
+    date: "۱۳۹۸-۰۶-۲۱ ۲۲:۵۴:۰۴",
+    update: "۱۳۹۸-۰۶-۲۱ ۲۳:۰۲:۲۲"
+  },
+  {
+    _id: mongoose.Types.ObjectId("5d7a8f5efabfe01afc47d24e"),
+    products: [
+      {
+        imgs: [],
+        _id: mongoose.Types.ObjectId("5d7a8cebfabfe01afc47c5ae"),
+        proCode: 1185,
+        marketCode: 780003,
+        name: "كفگير برگردون بامبو تكي  | زرد | چوبي | ماي كيچن | M",
+        description: "",
+        tradeBuyingPrice: 380000,
+        retailStoreStock: 0,
+        img: "http://www.taminmall.com/info/gateway/nepw/1185.1.jpg",
+        flag: "nikrad",
+        quantity: 1
+      },
+      {
+        imgs: [],
+        _id: mongoose.Types.ObjectId("5d7a8cebfabfe01afc47c5af"),
+        proCode: 1343,
+        marketCode: "253008",
+        name: "سرويس 6 پارچه كفگير و ملاقه  | سفيد | پلاستيك | ماي كيچن  | M",
+        description: "",
+        tradeBuyingPrice: 1230000,
+        retailStoreStock: 0,
+        img: "http://www.taminmall.com/info/gateway/nepw/1343.1.jpg",
+        flag: "nikrad",
+        quantity: 3
+      }
+    ],
+    name: "فاکتور خرید مربوط به سعید شمس",
+    invoiceType: "فاکتور خرید",
+    sellerName: "دانیال دانشی",
+    sellerAddress: "",
+    sellerPhoneNumber: "",
+    buyerId: mongoose.Types.ObjectId("5d7a8e03fabfe01afc47d24c"),
+    buyerName: "سعید شمس",
+    buyerAddress: "شوش، خیابان فدائیان اسلام، کوچه هفتم، پلاک 9، واحد2",
+    buyerPhoneNumber: "09122208579",
+    totalPrice: 4070000,
+    date: "۱۳۹۸-۰۶-۲۱ ۲۳:۰۳:۰۲",
+    update: "۱۳۹۸-۰۶-۲۱ ۲۳:۰۳:۰۲"
+  },
+  {
+    _id: mongoose.Types.ObjectId("5d7d020a7886d012c8648c32"),
+    products: [
+      {
+        imgs: [],
+        _id: mongoose.Types.ObjectId("5d7a8cebfabfe01afc47c5b0"),
+        proCode: 1377,
+        marketCode: "956313",
+        name: "شمعدان طرح چراغ بزرگ 523-W12668 | برنز | فلز | سلينا | FA",
+        description: "",
+        tradeBuyingPrice: 2780000,
+        retailStoreStock: 0,
+        img: "http://www.taminmall.com/info/gateway/nepw/1377.1.jpg",
+        flag: "nikrad",
+        quantity: 1
+      }
+    ],
+    name: "فاکتور فروش مربوط به محسن شمس",
+    invoiceType: "فاکتور فروش",
+    sellerName: "سعید شمس",
+    sellerAddress: "",
+    sellerPhoneNumber: "",
+    buyerId: mongoose.Types.ObjectId("5d7a8ed5fabfe01afc47d24d"),
+    buyerName: "محسن شمس",
+    buyerAddress: "شوش، خیابان فدائیان اسلام، کوچه هفتم، پلاک 9، واحد2",
+    buyerPhoneNumber: "09120275327",
+    totalPrice: 2780000,
+    date: "۱۳۹۸-۰۶-۲۳ ۱۹:۳۶:۵۰",
+    update: "۱۳۹۸-۰۶-۲۳ ۱۹:۳۶:۵۰"
+  }
+];
